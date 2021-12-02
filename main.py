125d8d48852af8a0129f75226a2798a006313bd8
MinekPo1 <68391527+MinekPo1@users.noreply.github.com>
2021-12-03 00:30:58 +0100

    added some stuff: - presence - reactions - deleting of own messages

--- main.py
+++ main.py
@@ -5,20 +5,19 @@ import requests
 
 dc = discord.Client()
 
-webhook = ("https://discord.com/api/webhooks/"
-	"916097212482744381/"
-	"0RRUdfJAgJVcf5TEKXQTVnlo3yVWSYeq91CwEFYZ3ePDeyB7IqApsvUMYSmg66r4e61F"
-)
-
 
 @dc.event
 async def on_ready():
-	requests.get(webhook, json={"content": "Bot is online."})
 	channel = dc.get_channel(805214055643349003)
 	if channel is not None:
-		channel.send("Bot is online!")
+		await channel.send("Bot is online!")
 	else:
 		print("Unable to access channel!")
+	await dc.change_presence(activity=discord.Activity(
+				type=discord.ActivityType.listening,
+				name=" to ppl who want to get a picrew link!",
+				details="reply to a message with search or react with :mag:!"
+	))
 	print("logged in as {}".format(dc.user))
 
 
@@ -26,7 +25,7 @@ async def on_ready():
 async def on_message(message:discord.Message):
 	if message.author == dc.user:
 		return
-	if "picrew" in message.channel.name and message.content == "search"\
+	if "picrew" in message.channel.name.lower() and message.content == "search"\
 			and message.reference is not None:
 		message = await message.channel.fetch_message(message.reference.message_id)
 		if message is None:
@@ -35,12 +34,60 @@ async def on_message(message:discord.Message):
 			for attachment in message.attachments:
 				# example file name: 1011016_hbWCv9R5.png
 				if m := search(r"^(\d+)_.+\.png$", attachment.filename):
-					await message.channel.send(
-						"https://picrew.me/image_maker/{}".format(m.group(1))
-					)
-					return
+					r = requests.get("https://picrew.me/image_maker/{}".format(m.group(1)))
+					if r.ok:
+						await message.channel.send(
+							"here: https://picrew.me/image_maker/{}".format(m.group(1))
+						)
+					else:
+						await message.channel.send(
+							"sadly got a {} :( picrew id: {}".format(r.status_code,m.group(1)),
+							# )
+							delete_after=5
+						)
 				else:
-					await message.channel.send("Unable to get picrew id, sorry :(.")
+					await message.channel.send(
+						"Unable to get picrew id, sorry :(.",
+						delete_after=5
+					)
+					# )
+				return
 
+
+@dc.event
+async def on_reaction_add(reaction:discord.Reaction, user:discord.User):
+	if "picrew" in reaction.message.channel.name.lower()\
+			and reaction.emoji == "🔍" and reaction.message.attachments is not None:
+		message = reaction.message
+		for attachment in message.attachments:
+			# example file name: 1011016_hbWCv9R5.png
+			if m := search(r"^(\d+)_.+\.png$", attachment.filename):
+				r = requests.get("https://picrew.me/image_maker/{}".format(m.group(1)))
+				if r.ok:
+					if user.permissions_in(message.channel).send_messages:
+						await message.channel.send(
+							"here: https://picrew.me/image_maker/{}".format(m.group(1)),
+							reference=message
+						)
+					else:
+						await message.channel.send(
+							"here: https://picrew.me/image_maker/{}".format(m.group(1)),
+							reference=message,
+							delete_after=5
+						)
+				else:
+					await message.channel.send(
+						"sadly got a {} :( picrew id: {}".format(r.status_code,m.group(1)),
+						# )
+						reference=message,
+						delete_after=5
+					)
+			else:
+				await message.channel.send(
+					"Unable to get picrew id, sorry :(.",
+					delete_after=5
+				)
+				# )
+			return
 
 dc.run(os.getenv("TOKEN"))
