600f59cadf1f68218d8bb4e673ae15826bc9f2b0
MinekPo1 <68391527+MinekPo1@users.noreply.github.com>
2021-12-03 20:21:22 +0100

    actualy fixed search by emote

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import os
 from re import search
-from typing import Union, cast
+from typing import Union, cast, overload
 import discord
 from discord.embeds import Embed
 import requests
+import sys
 
 dc = discord.Client()
 
 
 @dc.event
 async def on_ready():
+	if dc.user is None:
+		print("Failed to login.")
+		sys.exit(1)
 	channel = dc.get_channel(805214055643349003)
 	if channel is not None:
 		await channel.send("Bot is online!")
@@ -20,17 +24,45 @@ async def on_ready():
 				name=" to get a picrew link requests!"
 	))
 	print("logged in as {}".format(dc.user))
+	if "i" in sys.argv:
+		print(
+			"invite: {}".format(
+				discord.utils.oauth_url(dc.user.id, discord.Permissions(68608))
+			)
+		)
+
+
+@overload
+async def scan(
+		message:discord.Message,
+		channel:discord.TextChannel,
+		always_delete:bool = False,
+		reply:bool = False
+	) -> None:
+	...
+
+
+@overload
+async def scan(
+		message:str,
+		channel:discord.TextChannel,
+		always_delete:bool = False,
+	) -> None:
+	...
 
 
 async def scan(
 			message:Union[discord.Message,str],
 			channel:discord.TextChannel,
-			always_delete:bool = False
+			always_delete:bool = False,
+			reply:bool = False
 		):
 	id:str = ""
 	if isinstance(message,str) or message.attachments is None:
 		if not(isinstance(message,str)):
 			message = cast(str,message.content)
+		elif reply:
+			raise ValueError("Cannot reply to a string message!")
 		if m:=search(r"https?://.+/(\d+)_.+\.png",message):
 			id = m.group(1)
 	else:
@@ -41,34 +73,23 @@ async def scan(
 	if id:
 		r = requests.get("https://picrew.me/image_maker/{}".format(id))
 		if r.ok:
-			if always_delete:
-				await channel.send(
-					embed=Embed(
-						description="here: https://picrew.me/image_maker/{}".format(id),
-					),
-					delete_after=5
-				)
-			else:
-				await channel.send(
-					embed=Embed(
-						description="here: https://picrew.me/image_maker/{}".format(id)
-					)
-				)
+			mess="here: https://picrew.me/image_maker/{}".format(id)
+			delete = always_delete
 		else:
-			await channel.send(
-				embed=Embed(
-					descition="sadly got a {} :( picrew id: {}".format(r.status_code,id),
-					# )
-				),
-				delete_after=5
-			)
+			mess ="sadly got a {} :( picrew id: {}".format(r.status_code,id)  # )
+			delete = True
 	else:
-		await channel.send(
+		mess="Unable to get picrew id, sorry :(."  # )
+		delete = True
+	await channel.send(
 			embed=Embed(
-				descition="Unable to get picrew id, sorry :(."
-				# )
+				description=mess
 			),
-			delete_after=5
+			mention_author=False,
+			**{
+				"delete_after": 5 if delete else None,
+				"reference": message if reply else None
+			}
 		)
 
 
@@ -116,23 +137,47 @@ async def on_message(message:discord.Message):
 async def on_raw_reaction_add(data:discord.RawReactionActionEvent):
 	if data.event_type != "REACTION_ADD":
 		return
+	print("got reaction:{!r}".format(data.emoji))
 
 	channel = dc.get_channel(data.channel_id)
 	if channel is None:
 		return
 	message = await channel.fetch_message(data.message_id)
+
 	if not(isinstance(message,discord.Message)):
 		return
 
+	print("got message:",message)
+	print("has attachments:",message.attachments is not None)
+
 	if "picrew" in channel.name.lower()\
-			and data.emoji == "🔍" and message.attachments is not None:
+			and data.emoji.name == "🔍" and message.attachments is not None:
 		user = dc.get_user(data.user_id)
+		print("off to scan...")
 		await scan(
 			message, message.channel,
 			always_delete=(
 				user is not None
 				and not(user.permissions_in(message.channel).send_messages)
-			)
+			),
+			reply=True
 		)
 
-dc.run(os.getenv("TOKEN"))
+token = os.getenv("TOKEN")
+
+if token is None:
+	try:
+		with open('.env', 'r') as fh:
+			vars_dict = {
+				line.split('=')[0]: line.split('=')[1].strip()
+				for line in fh.readlines() if not line.startswith('#')
+			}
+		if 'TOKEN' in vars_dict:
+			token = vars_dict['TOKEN']
+		else:
+			print("Unable to get token via getenv or from .env file!")
+	except FileNotFoundError:
+		print("Unable to get token via getenv and no .env file found!")
+		exit(1)
+
+dc.run(token)
